896ed0cac49552973a257e20cb2cc21924e71f02
Mehdi Lahlou <lahlou.mehdi@gmail.com>
2015-10-11 16:10:45 +0200

    Update server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -1,3 +1,4 @@
+/* 
 import vorlonServer = require("./Scripts/vorlon.server");
 import vorlonDashboard = require("./Scripts/vorlon.dashboard");
 import vorlonWebserver = require("./Scripts/vorlon.webServer");
@@ -18,12 +19,12 @@ var HttpProxy = new vorlonHttpProxy.VORLON.HttpProxy(false);
 webServer.components.push(dashboard);
 webServer.components.push(server);
 webServer.components.push(HttpProxy);
-webServer.start();
+webServer.start(); */
 
 // comment above to deploy a standalone instance of httpProxy
 // and uncomment the following
 
-/*import httpproxyServer = require("./Scripts/vorlon.httpproxy.server");
+import httpproxyServer = require("./Scripts/vorlon.httpproxy.server");
 
 var server = new httpproxyServer.VORLON.HttpProxy(true);
-server.start();*/
+server.start();
