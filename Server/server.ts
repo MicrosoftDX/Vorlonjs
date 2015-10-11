6accb0408def6eaac4d56673e5f0d0da4cff95b7
Mehdi Lahlou <lahlou.mehdi@gmail.com>
2015-10-11 16:39:08 +0200

    Update server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -1,25 +1,25 @@
-/* 
-import vorlonServer = require("./Scripts/vorlon.server");
-import vorlonDashboard = require("./Scripts/vorlon.dashboard");
-import vorlonWebserver = require("./Scripts/vorlon.webServer");
-import vorlonHttpProxy = require("./Scripts/vorlon.httpproxy.server");
+// import vorlonServer = require("./Scripts/vorlon.server");
+// import vorlonDashboard = require("./Scripts/vorlon.dashboard");
+// import vorlonWebserver = require("./Scripts/vorlon.webServer");
+// import vorlonHttpProxy = require("./Scripts/vorlon.httpproxy.server");
+// 
+// //WEBSERVER
+// var webServer = new vorlonWebserver.VORLON.WebServer();
+// 
+// //DASHBOARD
+// var dashboard = new vorlonDashboard.VORLON.Dashboard();
+// 
+// //VORLON SERVER
+// var server = new vorlonServer.VORLON.Server();
+// 
+// //VORLON HTTPPROXY
+// var HttpProxy = new vorlonHttpProxy.VORLON.HttpProxy(false);
+// 
+// webServer.components.push(dashboard);
+// webServer.components.push(server);
+// webServer.components.push(HttpProxy);
+// webServer.start();
 
-//WEBSERVER
-var webServer = new vorlonWebserver.VORLON.WebServer();
-
-//DASHBOARD
-var dashboard = new vorlonDashboard.VORLON.Dashboard();
-
-//VORLON SERVER
-var server = new vorlonServer.VORLON.Server();
-
-//VORLON HTTPPROXY
-var HttpProxy = new vorlonHttpProxy.VORLON.HttpProxy(false);
-
-webServer.components.push(dashboard);
-webServer.components.push(server);
-webServer.components.push(HttpProxy);
-webServer.start(); */
 
 // comment above to deploy a standalone instance of httpProxy
 // and uncomment the following
