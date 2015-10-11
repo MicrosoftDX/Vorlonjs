552c1435b2db739a3bb40d63b5fcb6cb28bf1942
Mehdi Lahlou <lahlou.mehdi@gmail.com>
2015-10-11 15:55:34 +0200

    Update vorlon.httpconfig.ts

--- Server/config/vorlon.httpconfig.ts
+++ Server/config/vorlon.httpconfig.ts
@@ -34,7 +34,7 @@ export module VORLON {
                 this.protocol = "http";
                 this.httpModule = http;
             }
-            if(catalog.proxyEnvPort && process.env.PORT)
+            if(catalog.proxyEnvPort)
                 this.proxyPort = process.env.PORT;  
             else
                 this.proxyPort = catalog.proxyPort || 5050;         
