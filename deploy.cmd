70d2b7433882351932e648a284a233d22ee3a56d
Etienne Margraff <etienne.margraff@live.fr>
2015-11-17 19:02:33 +0100

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -100,6 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install –g npm@3.0-latest
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
