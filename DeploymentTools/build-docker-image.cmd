ed3eaba4fdfa84f7fb3f38fb167c413ac8b0bf35
Julien Corioland <julien.corioland@live.fr>
2016-02-01 21:42:17 +0100

    update docker build file

--- DeploymentTools/build-docker-image.cmd
+++ DeploymentTools/build-docker-image.cmd
@@ -10,34 +10,13 @@ IF "%5"=="" GOTO :usage
 SET DOCKER_HOST=%1
 
 @ECHO "BUILD DOCKER IMAGE"
-<<<<<<< e8b74930c52175581277487b80f1b7f5ef039cd5
-<<<<<<< 1ddaafe72dfdf26ab009d539b8def790548b149c
-<<<<<<< 992816301884e11deb56b1f1704453798c451657
 docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" build -t jcorioland/vorlonjs:0.2.1
-=======
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" build -t jcorioland/vorlonjs:0.2 .
->>>>>>> Fixing for 0.2
-=======
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" build -t jcorioland/vorlonjs:0.2.1 .
->>>>>>> update docker image script
-=======
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" build -t jcorioland/vorlonjs:0.2.1
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" tag jcorioland/vorlonjs:latest jcorioland/vorlonjs:0.2.1
->>>>>>> update docker image to add "latest" tag
 
 @ECHO "LOG INTO DOCKER HUB"
 docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" login --username="%3" --password="%4" --email="%5" https://index.docker.io/v1/
 
 @ECHO "PUSH IMAGE INTO DOCKER HUB"
-<<<<<<< 1ddaafe72dfdf26ab009d539b8def790548b149c
-<<<<<<< 992816301884e11deb56b1f1704453798c451657
 docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" push jcorioland/vorlonjs:0.2.1
-=======
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" push jcorioland/vorlonjs:0.2
->>>>>>> Fixing for 0.2
-=======
-docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" push jcorioland/vorlonjs:0.2.1
->>>>>>> update docker image script
 
 @ECHO "LOG OUT FROM DOCKER HUB."
 docker --tls --tlscacert="%2\ca.pem" --tlscert="%2\cert.pem" --tlskey="%2\key.pem" logout
