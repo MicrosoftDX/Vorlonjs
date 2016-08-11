2878c86a9e1275fbf6474cae71b721fa4aa60b53
Mike Sandt <michael.sandt@gmail.com>
2016-08-11 13:48:00 -0400

    WIP for service object to provide config file from CLI

---
+++ Server/config/vorlon.configprovider.ts
@@ -0,0 +1,10 @@
+import fs = require("fs");
+import path = require("path");
+
+var argv = require('minimist')(process.argv.slice(2));
+
+export module VORLON {
+  export class ConfigProvider {
+
+  }
+}
