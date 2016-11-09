ed82ba1abbb73ad210cd463bc5a52cf09582563b
Etienne Margraff <etmargra@microsoft.com>
2016-11-09 14:14:04 -0800

    First draft with hooking for libraries

--- Plugins/Vorlon/plugins/botFrameworkInspector/vorlon.botFrameworkInspector.dashboard.ts
+++ Plugins/Vorlon/plugins/botFrameworkInspector/vorlon.botFrameworkInspector.dashboard.ts
@@ -6,17 +6,29 @@
             this._id = "BOTFRAMEWORKINSPECTOR";
         }
 
+        private _lastReceivedBotInfo: BotInfo;
         private _containerDiv: HTMLElement;
         
         public startDashboardSide(div: HTMLDivElement = null): void {
             this._insertHtmlContentAsync(div, (filledDiv) => {
                 this._containerDiv = Tools.QuerySelectorById(div, "botFrameworkInspector");
                 this._ready = true;
+                this.display();
             })
         }
 
         public onRealtimeMessageReceivedFromClientSide(receivedObject:any):void {
-            this._containerDiv.textContent = receivedObject;
+            this._lastReceivedBotInfo = receivedObject;
+            this.display();
+        }
+        
+        public display(){
+            if(this._lastReceivedBotInfo){
+                this._containerDiv.textContent = "";
+                for(var i = 0; i < this._lastReceivedBotInfo.dialogDataList.length; i++){
+                    this._containerDiv.textContent += "<br />" + this._lastReceivedBotInfo.dialogDataList[i].id;
+                }
+            }
         }
     }
 
