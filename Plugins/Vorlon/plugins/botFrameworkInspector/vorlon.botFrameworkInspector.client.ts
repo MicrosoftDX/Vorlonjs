6e9feba2d988cc0761bef945f19cd6486bab63fb
Etienne Margraff <etmargra@microsoft.com>
2016-11-08 16:51:30 -0800

    Init the Botframework inspector plugin

---
+++ Plugins/Vorlon/plugins/botFrameworkInspector/vorlon.botFrameworkInspector.client.ts
@@ -0,0 +1,24 @@
+﻿module VORLON {
+    export class BotFrameworkInspectorClient extends ClientPlugin {
+        constructor() {
+            super("botFrameworkInspector");
+            //this.debug = true;
+            this._ready = true;
+        }
+
+        public getID(): string {
+            return "BOTFRAMEWORKINSPECTOR";
+        }
+
+        public sendClientData(): void {
+            this.sendToDashboard("Yo bitcheeeeeeezzZZZZ!");            
+        }
+
+        public refresh(): void {
+            this.sendClientData();
+        }
+    }
+
+    //Register the plugin with vorlon core 
+    Core.RegisterClientPlugin(new BotFrameworkInspectorClient());
+} 
---
+++ Plugins/Vorlon/plugins/botFrameworkInspector/vorlon.botFrameworkInspector.dashboard.ts
@@ -0,0 +1,25 @@
+﻿module VORLON {
+    export class BotFrameworkInspectorDashboard extends DashboardPlugin {
+        constructor() {
+            super("botFrameworkInspector", "control.html", "control.css");
+            this._ready = false;
+            this._id = "BOTFRAMEWORKINSPECTOR";
+        }
+
+        private _containerDiv: HTMLElement;
+        
+        public startDashboardSide(div: HTMLDivElement = null): void {
+            this._insertHtmlContentAsync(div, (filledDiv) => {
+                this._containerDiv = Tools.QuerySelectorById(div, "botFrameworkInspector");
+                this._ready = true;
+            })
+        }
+
+        public onRealtimeMessageReceivedFromClientSide(receivedObject:any):void {
+            this._containerDiv.textContent = receivedObject;
+        }
+    }
+
+    //Register the plugin with vorlon core 
+    Core.RegisterDashboardPlugin(new BotFrameworkInspectorDashboard());
+} 
