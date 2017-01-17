f1074141aafb4a121e060031808e1379d5b1742b
mboelter <m@rebooten.de>
2017-01-17 12:48:20 +0800

    Update vorlon.dashboardManager.ts
    Images on sidebar to respect baseURL from config

--- Server/public/vorlon.dashboardManager.ts
+++ Server/public/vorlon.dashboardManager.ts
@@ -149,7 +149,7 @@ module VORLON {
             }
             
             var pluginlistelementa = document.createElement("a");
-            pluginlistelementa.innerHTML = " <img src='/images/systems/"+client.icon+"' alt='icon_system'> " + (client.identity ? client.identity : client.name) + " - " + client.displayid;
+            pluginlistelementa.innerHTML = " <img src='" + DashboardManager.vorlonBaseURL + "/images/systems/"+client.icon+"' alt='icon_system'> " + (client.identity ? client.identity : client.name) + " - " + client.displayid;
             pluginlistelementa.setAttribute("href", vorlonBaseURL + "/dashboard/" + DashboardManager.SessionId + "/" + client.clientid);
             pluginlistelement.appendChild(pluginlistelementa);
 
